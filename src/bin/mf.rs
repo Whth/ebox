2abdc8cfbf57c5b560fc26e0fbf8a51eceef2a12
Whth <whthyotta@qq.com>
2024-11-17 20:46:39 +0800

    feat(mf): better match

--- src/bin/mf.rs
+++ src/bin/mf.rs
@@ -2,10 +2,12 @@ use clap::Parser;
 use dialoguer::{Confirm, Select};
 use fs_extra::dir::CopyOptions;
 use humansize::{format_size, BaseUnit, FormatSizeOptions};
+use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::{self};
 use std::path::PathBuf;
+use strsim::normalized_damerau_levenshtein;
 use walkdir::WalkDir;
 
 #[derive(Parser)]
@@ -97,16 +99,18 @@ fn move_files(opt: &CopyOptions, dst_full_path: &String, to_move: &[PathBuf]) {
     let mut cur_file = String::new();
     let mut done = 0u64;
     let size_opt = FormatSizeOptions::default()
+        .decimal_zeroes(2)
+        .decimal_places(2)
         .base_unit(BaseUnit::Byte);
     fs_extra::move_items_with_progress(to_move,
                                        dst_full_path, opt,
                                        |prog|
                                            {
                                                (cur_file != prog.file_name).then(|| {
+                                                   print!("\r[{}/{}]Moving {} to {}", format_size(done, size_opt), format_size(prog.total_bytes, size_opt), prog.file_name, dst_full_path);
                                                    cur_file = prog.file_name.to_string();
                                                    done += prog.file_total_bytes;
                                                });
-                                               print!("\r[{}/{}]Moving {} to {}", format_size(done, size_opt), format_size(prog.file_total_bytes, size_opt), prog.file_name, dst_full_path);
                                                fs_extra::dir::TransitProcessResult::ContinueOrAbort
                                            })
         .expect("Failed to merge folders");
@@ -150,15 +154,71 @@ fn get_subfolders(folder: &str) -> Vec<String> {
         .collect()
 }
 
+trait RemoveBrackets {
+    fn remove_s_brackets(&self) -> String;
+    fn remove_m_brackets(&self) -> String;
+    fn remove_l_brackets(&self) -> String;
+}
+
+impl RemoveBrackets for str {
+    fn remove_s_brackets(&self) -> String {
+        let re = Regex::new(r"\(\D*\d*\D+\d*\D*\)").unwrap();
+        re.replace_all(self, "").into_owned()
+    }
+
+    fn remove_m_brackets(&self) -> String {
+        let re = Regex::new(r"\[\D*\d*\D+\d*\D*]").unwrap();
+        re.replace_all(self, "").into_owned()
+    }
+
+    fn remove_l_brackets(&self) -> String {
+        let re = Regex::new(r"\{\D*\d*\D+\d*\D*}").unwrap();
+        re.replace_all(self, "").into_owned()
+    }
+}
+
+trait Uid {
+    fn uid(&self) -> Option<usize>;
+
+    fn eq_uid(&self, other: &Self) -> bool;
+}
+impl Uid for str {
+    fn uid(&self) -> Option<usize> {
+        let re = Regex::new(r"(\d{6,})").unwrap();
+        if re.find(self).is_some() {
+            Some(re.find(self).unwrap().as_str().parse::<usize>().unwrap())
+        } else {
+            None
+        }
+    }
+
+    fn eq_uid(&self, other: &str) -> bool {
+        if let (Some(self_uid_val), Some(other_uid_val)) = (self.uid(), other.uid()) {
+            self_uid_val == other_uid_val
+        } else {
+            false
+        }
+    }
+}
+
+fn eval_similarity(src: &str, dst: &str) -> f64 {
+    normalized_damerau_levenshtein(src, dst)
+        .max(normalized_damerau_levenshtein(src.remove_m_brackets()
+                                                .remove_l_brackets()
+                                                .remove_s_brackets().as_str(), dst))
+        .max(if src.eq_uid(dst) { 1. } else { 0. })
+}
+
 /// find possible matches for a given string in a list of strings
 fn find_possible_matches(src: &str, matches: &[String], threshold: f64) -> Vec<(String, usize)> {
     let mut possible_matches: Vec<_> = matches
         .iter()
-        .map(|dir| (dir.to_owned(), (strsim::normalized_damerau_levenshtein(src, dir) * 100.) as usize))
+        .map(|dir| (dir.to_owned(), (eval_similarity(src, dir) * 100.) as usize))
         .collect();
 
-    // 按 Levenshtein 距离从小到大排序
+    // 按 Levenshtein 距离从高到低排序
     possible_matches.sort_by_key(|(_, score)| -(*score as isize));
+
 
     let end = possible_matches.iter().position(|(_, score)| score < &((threshold * 100.) as usize)).unwrap_or(possible_matches.len());
     possible_matches.truncate(end);
