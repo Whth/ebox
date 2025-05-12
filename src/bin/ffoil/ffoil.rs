d498ed0e4b27c74784749e6b6776655d84b030a7
Whth <whthyotta@qq.com>
2025-05-12 09:14:54 +0800

    refactor(ffoil): update max thickness series for NACA 4-digit airfoils
    - Extend the default value for max thickness series to include all available options (1 to 99)- Improve code readability by splitting the long default value string across multiple lines

--- src/bin/ffoil/ffoil.rs
+++ src/bin/ffoil/ffoil.rs
@@ -143,7 +143,7 @@ struct SearchNacaArgs {
     camber_pos: Vec<u8>,
 
     /// Max thickness percentages (XX) for NACA 4-digit series (e.g., "06,12,18").
-    #[arg(long, value_parser = clap::value_parser!(u8), num_args = 1.., value_delimiter = ',', default_value = "01,02,03,04,05,06,07,08,09\
+    #[arg(long, value_parser = clap::value_parser!(u8), num_args = 1.., value_delimiter = ',', default_value = "01,02,03,04,05,06,07,08,09,\
         10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,\
         56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99"
     )]
